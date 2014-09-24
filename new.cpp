998134a465fce251534c406b811ad08c634aba04
unknown <lawingerje@WKS004366.ion.uwplatt.edu>
2014-09-24 17:24:01 -0500

    Added new.cpp

--- new.cpp
+++ new.cpp
@@ -1,12 +1,5 @@
 int main() {
-	cout << "Hello World!";
-	int i = 0;
-<<<<<<< HEAD
-=======
-	
-	if (i == 0) {
-		cout << "All tests passed.";
-	}
->>>>>>> Testing
-	return 0;
+   cout << "Hello World!";
+   int i = 0;
+   return 0;
 }
